6bc1071f4ce37e569b8237b173779e0db54b6829
Jacek <jgebal@users.noreply.github.com>
2017-06-20 23:27:05 +0100

    Adding travis config for the project

---
+++ .travis/install_utplsql_v2.3.1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ev
+
+cd ${UTPLSQL_V2_DIR}/code
+
+sqlplus -L -S / AS SYSDBA <<SQL
+create user utp identified by utp default tablespace
+  users temporary tablespace temp;
+
+grant create session, create table, create procedure,
+  create sequence, create view, create public synonym,
+  drop public synonym to utp;
+
+alter user utp quota unlimited on users;
+exit
+SQL
+
+sqlplus -S -L / AS SYSDBA @ut_i_do.sql install
