2d05f1b57898e39f914884effdd4790f0c995c36
Jacek Gebal <jgebal@users.noreply.github.com>
2018-09-27 01:10:40 +0100

    Fixing build issues with Travis-Oracle

--- source/install_component.sql
+++ source/install_component.sql
@@ -1,7 +1,8 @@
 set heading off
 set feedback off
-exec dbms_output.put_line('Installing component '||upper(regexp_substr('&&1','\/(\w*)\.',1,1,'i',1)));
-@@&&1
+def component = &&1
+exec dbms_output.put_line('Installing component '||upper(regexp_substr('&&component','\/(\w*)\.',1,1,'i',1)));
+@@&&component
 show errors
 exec dbms_output.put_line('--------------------------------------------------------------');
-
+undef component
